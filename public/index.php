d804fff8faf82247651ac81597df82555c23454e
Humberto <jairhumberto@gmail.com>
2017-12-10 01:04:12 -0200

    Corrections on initialization point

--- public/index.php
+++ public/index.php
@@ -1,22 +1,18 @@
 <?php
 session_start();
 
-ini_set('display_errors', 'On');
-
-error_reporting(E_ALL - E_NOTICE);
-
-require_once ('../vendor/Squille/Core/helpers/constants.php');
-
-$loader = require_once (VENDOR_DIR . 'autoload.php');
-$loader->add(null, VENDOR_DIR);
-$loader->add(null, SOURCE_DIR);
-
 use Squille\Core\System;
 use Squille\Core\Collection;
 
 ini_set('max_execution_time', 0);
 
-$config = include CONFIG_DIR . 'config.php';
+require '..'
+      . DIRECTORY_SEPARATOR . 'vendor'
+      . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$config = include '..'
+                . DIRECTORY_SEPARATOR . 'source'
+                . DIRECTORY_SEPARATOR . 'config.php';
 
 if (empty($_FILES)) {
     $args = new Collection($_REQUEST);
