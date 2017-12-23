a42e28cdeba8e7b2c4c910b9ebb2886ffb4b796e
Humberto <jairhumberto@gmail.com>
2017-12-22 23:12:16 -0200

    Removed the routes-sample as it doesnt make sense

---
+++ source/Admin/routes.php
@@ -0,0 +1,15 @@
+<?php
+// Routing per module.
+return array(
+    '/auth' => 'Admin\Controller\AuthController',
+    '/categories' => 'Admin\Controller\CategoriesController',
+    '/contents' => 'Admin\Controller\ContentsController',
+    '/' => 'Admin\Controller\IndexController',
+    '/links' => 'Admin\Controller\LinksController',
+    '/menus' => 'Admin\Controller\MenusController',
+    '/pages-categories' => 'Admin\Controller\PagesCategoriesController',
+    '/pages-contents' => 'Admin\Controller\PagesContentsController',
+    '/pages' => 'Admin\Controller\PagesController',
+    '/pages-menus' => 'Admin\Controller\PagesMenusController',
+    '/files' => 'Admin\Controller\FilesController'
+);
